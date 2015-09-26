57c5b9ed321d1662850dc7a6b212949af6dfa849
Daniela Baron <baron.daniela@gmail.com>
2015-09-26 16:59:56 -0400

    use http client to get a list of movies

--- movies/src/app.js
+++ movies/src/app.js
@@ -1,18 +1,18 @@
+import {HttpClient} from 'aurelia-http-client';
+
 export class App {
 
   constructor() {
     this.message = '';
+    this.http = new HttpClient(); // later will inject
   }
 
   activate() {
     this.message = 'Hello from view model activate';
+    return this.http.get('/api/movies.json')
+      .then(response => {
+        this.movies = response.content;
+      });
   }
 
-  changeMessage() {
-    if (this.message === 'Goodbye') {
-      this.message = 'Hello';
-    } else {
-      this.message = 'Goodbye';
-    }
-  }
 }
