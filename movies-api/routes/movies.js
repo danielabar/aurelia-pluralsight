fe62a0aca0452c062c30168ed1b56d0bd9982389
Daniela Baron <baron.daniela@gmail.com>
2015-09-27 09:17:50 -0400

    api create a movie

--- movies-api/routes/movies.js
+++ movies-api/routes/movies.js
@@ -32,8 +32,24 @@ router.get('/:id', function(req, res) {
   if (matchingMovie) {
     res.send(matchingMovie);
   } else {
-    res.json(404, {error: {message: 'We did not find a movie with id: ' + req.params.id } });
+    res.send(404, {error: {message: 'We did not find a movie with id: ' + req.params.id } });
   }
+});
+
+router.post('/', function(req, res) {
+  if (!req.body.title) {
+    res.send(400, {error: {message: 'A title is required to create a new movie.'} });
+    return;
+  }
+
+  var created = {
+    id: 'tbd',
+    title: req.body.title,
+    releaseYear: req.body.releaseYear,
+  };
+  moviesList.push(created);
+
+  res.send(201, created);
 });
 
 module.exports = router;
