d6bea73fce64341a33bdb4698b79f326a36de033
Daniela Baron <baron.daniela@gmail.com>
2016-01-23 14:58:45 -0500

    fix movie service http config

--- hello-aurelia/public/movie/movie-service.js
+++ hello-aurelia/public/movie/movie-service.js
@@ -5,18 +5,17 @@ import {HttpClient} from 'aurelia-fetch-client';
 export class MovieService {
 
   constructor(httpClient) {
-    http.configure(config => {
+    this.http = httpClient;
+    this.http.configure(config => {
       config
         .useStandardConfiguration()
         .withBaseUrl('http://localhost:3000/');
     });
-
-    this.http = httpClient;
   }
 
   fetchAll() {
     return this.http.fetch('movies')
-      .then(response => response.json())
+      .then(response => response.json() );
   }
 
 }
