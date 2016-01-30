5aea401b3ebb397243cd0f7841880ecb8a5e73da
Daniela Baron <baron.daniela@gmail.com>
2016-01-30 09:33:27 -0500

    prepopulate datepicker

--- movies-client/public/components/datepicker.js
+++ movies-client/public/components/datepicker.js
@@ -2,6 +2,7 @@ import {inject, customElement, bindable} from 'aurelia-framework';
 import $ from 'jquery';
 import 'bootstrap-datepicker/dist/js/bootstrap-datepicker';
 import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.css!';
+import moment from 'moment';
 
 @customElement('datepicker')
 @inject(Element)
@@ -17,10 +18,24 @@ export class DatePicker {
   }
 
   attached() {
+    // Find input element
     let selector = $(this.element).find('.date');
+
+    // Initialize datepicker
     selector.datepicker({
       format: 'yyyy-mm-dd',
       autoclose: true
     });
+
+    // Update bound data on selection
+    selector.datepicker().on('changeDate', (e) => {
+      // could also say: this.data = e.format();
+      this.data = moment(e.date).format('YYYY-MM-DD');
+    });
+
+    // Prepopulate with bound data
+    if (this.data) {
+      selector.datepicker('setDate', moment(this.data, 'YYYY-MM-DD').toDate());
+    }
   }
 }
--- movies-client/public/config.js
+++ movies-client/public/config.js
@@ -27,6 +27,7 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.20",
     "font-awesome": "npm:font-awesome@4.5.0",
     "jquery": "npm:jquery@2.2.0",
+    "moment": "npm:moment@2.11.1",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -187,6 +188,9 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:moment@2.11.1": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
